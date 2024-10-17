3bf5ebd2450bfd6270072f71df86162c00eae191
Musabdulhim3 <musabdulhim3@gmail.com>
2024-10-17 10:06:03 +0100

    moved from streaming via WebView to ExoPlayer

--- app/src/main/java/net/thebookofcode/www/fyp/ui/HomeFragment.kt
+++ app/src/main/java/net/thebookofcode/www/fyp/ui/HomeFragment.kt
@@ -9,15 +9,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.MimeTypes
 import net.thebookofcode.www.fyp.R
 import net.thebookofcode.www.fyp.databinding.FragmentHomeBinding
 import net.thebookofcode.www.fyp.util.Constants.Companion.BASE_URL
+import net.thebookofcode.www.fyp.util.Constants.Companion.STREAMING_FILE
+import net.thebookofcode.www.fyp.util.Constants.Companion.STREAMING_PATH
 import net.thebookofcode.www.fyp.util.Constants.Companion.STREAMING_PORT
 
 
 class HomeFragment : Fragment() {
-    private var _binding:FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+
+    private val liveStreamUrl = "$BASE_URL/$STREAMING_PATH/$STREAMING_FILE"
 
 
     override fun onCreateView(
@@ -25,20 +35,22 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.llSettings.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_settingsFragment)
         }
 
         binding.llNotifications.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_notificationListFragment)
+            Navigation.findNavController(it)
+                .navigate(R.id.action_homeFragment_to_notificationListFragment)
         }
 
         binding.llSnapshots.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_snapshotsFragment)
         }
         binding.llVideoView.setOnClickListener {
-            playLiveVideo()
+            //playLiveVideo()
+            playLiveStream()
         }
 
         return binding.root
@@ -47,13 +59,38 @@ class HomeFragment : Fragment() {
     private fun playLiveVideo() {
         val videoUri = Uri.parse("$BASE_URL:$STREAMING_PORT")
         val videoUrl = "$BASE_URL:$STREAMING_PORT"
+        binding.videoView.visibility = View.VISIBLE
+        binding.exoPlayerView.visibility = View.GONE
         binding.videoView.loadUrl(videoUrl)
         // binding.videoView.setVideoURI(videoUri)
+    }
+
+    private fun playLiveStream() {
+        binding.videoView.visibility = View.GONE
+        binding.exoPlayerView.visibility = View.VISIBLE
+        exoPlayer = ExoPlayer.Builder(requireContext()).build()
+        binding.exoPlayerView.player = exoPlayer
+        val mediaItem = MediaItem.Builder()
+            .setUri(liveStreamUrl)
+            .setMimeType(MimeTypes.APPLICATION_M3U8)
+            .build()
+        exoPlayer?.setMediaItem(mediaItem)
+        exoPlayer?.prepare()
+        exoPlayer?.playWhenReady = true
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.contentPosition
+            player.release()
+            exoPlayer = null
+        }
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        releasePlayer()
     }
 }
--- app/src/main/java/net/thebookofcode/www/fyp/util/Constants.kt
+++ app/src/main/java/net/thebookofcode/www/fyp/util/Constants.kt
@@ -1,9 +1,11 @@
 package net.thebookofcode.www.fyp.util
 
-class Constants{
-    companion object{
-        const val BASE_URL = "http://192.168.228.220"
+class Constants {
+    companion object {
+        const val BASE_URL = "http://192.168.124.220"
         const val STREAMING_PORT = "8081"
+        const val STREAMING_FILE = "playlist.m3u8"
+        const val STREAMING_PATH="livestream/feeds"
         const val WEB_PORT = "8000"
     }
 }
